2c6b50871fc44d798216e977509658b85a684393
Hello World <szhou42@illinois.edu>
2020-09-29 14:56:11 -0700

    Create rabinkarp.py

---
+++ rabinkarp.py
@@ -0,0 +1,31 @@
+class RabinKarp:
+    def __init__(self, pattern):
+        self.Q = 113
+        self.M = len(pattern)
+        self.R = 256
+        
+        self.RM = 1
+        for i in range(self.M-1):
+            self.RM = (self.RM * self.R) % self.Q
+        self.pattern_hash = self.hash(pattern, self.M)
+        
+    def hash(self, text, M):
+        h = 0
+        for j in range(M):
+            h = (self.R * h + ord(text[j])) % self.Q
+        return h
+        
+    def search(self, s):
+        N = len(s)
+        s_hash = self.hash(s, self.M)
+        if s_hash == self.pattern_hash:
+            return 0
+        
+        for i in range(self.M, N):
+            s_hash = (s_hash + self.Q - (ord(s[i-self.M]) * self.RM) % self.Q) % self.Q
+            s_hash = (s_hash * self.R + ord(s[i])) % self.Q
+            if s_hash == self.pattern_hash:
+                return i - self.M + 1
+        return -1
+rk = RabinKarp("123")
+print(rk.search("abcde123f")) # should print 5
